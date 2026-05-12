f1ff2394b00880bbc475d1384e4188c4a3d52f52
kushagra9926 <joshikushagra899@gmail.com>
2026-05-12 12:33:56 +0530

    docs: add map() method examples with syntax and usage details

--- JavaScript/Lecture 8.js
+++ JavaScript/Lecture 8.js
@@ -65,3 +65,59 @@ Sam
 Alex
 */
 
+// 2. Map
+/*
+map() is used to create a new array by transforming each element of an existing array.
+
+SYNTAX: 
+array.map((element, index, array) => { });
+*/
+
+// BASIC EXAMPLE
+let nums = [1, 2, 3];
+
+let doubled = nums.map((num) => {
+  return num * 2;
+});
+console.log(nums);
+console.log(doubled);
+// Output: 
+/*
+[1, 2, 3]
+[2, 4, 6]
+*/
+
+// Important Point: map() does NOT change original array.
+
+// EXAMPLE WITH INDEX
+let fruits = ["apple", "banana", "mango"];
+
+let result = fruits.map((fruit, index) => {
+  return `${index} : ${fruit}`;
+});
+
+console.log(result);
+// Output: 
+/*
+[
+  "0 : apple",
+  "1 : banana",
+  "2 : mango"
+]
+*/
+
+// map() WITH OBJECTS
+let users = [
+  { name: "John", age: 20 },
+  { name: "Sam", age: 25 }
+];
+
+let names = users.map((user) => {
+  return user.name;
+});
+
+console.log(names);
+// Output: 
+/*
+["John", "Sam"]
+*/
