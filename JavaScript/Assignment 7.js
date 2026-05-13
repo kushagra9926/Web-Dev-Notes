f558ce7134ac38e396010d7ffd92b13853875a24
kushagra9926 <joshikushagra899@gmail.com>
2026-05-13 13:25:59 +0530

    docs: add solution for Question 3 in Assignment 7 JavaScript file

--- JavaScript/Assignment 7.js
+++ JavaScript/Assignment 7.js
@@ -26,5 +26,9 @@
     console.log(arr_3.map((arr) => arr + 5));
 
 // Question 3: Create a new array whose elements are in upper case of words present in the original array.
+// Solution:
+    let arr4 = ["John", "Max", "Rice", "Wet"];
+    console.log(arr4.map((arr) => arr.toUpperCase()));
+
 // Question 4: Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of the additional arguments doubled.
 // Question 5: Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
