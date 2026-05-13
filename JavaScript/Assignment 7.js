7d8a33fb5365ff98c3ac0d732642f268f7ea4ad7
kushagra9926 <joshikushagra899@gmail.com>
2026-05-13 13:18:56 +0530

    docs: add solution for Question 2 in Assignment 7 JavaScript file

--- JavaScript/Assignment 7.js
+++ JavaScript/Assignment 7.js
@@ -20,8 +20,11 @@
     let average_2 = sum / arr_2.length;
     console.log(average_2);
 
-    
 // Question 2: Create a new array using the map function whose each element is equal to the original element plus 5.
+// Solution: 
+    let arr_3 = [1, 2, 3, 4, 5];
+    console.log(arr_3.map((arr) => arr + 5));
+
 // Question 3: Create a new array whose elements are in upper case of words present in the original array.
 // Question 4: Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of the additional arguments doubled.
 // Question 5: Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
