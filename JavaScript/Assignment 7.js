260a83f78402bac761c7db0af4f51f41688d4ca0
kushagra9926 <joshikushagra899@gmail.com>
2026-05-13 14:30:23 +0530

    docs: add solution for Question 5 in Assignment 7 JavaScript file

--- JavaScript/Assignment 7.js
+++ JavaScript/Assignment 7.js
@@ -39,3 +39,9 @@
     doubleAndReturnArgs([1, 2, 3], 4, 4);
     doubleAndReturnArgs([2], 10, 4);
 // Question 5: Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+// Solution: 
+    const mergeObjects = (obj1, obj2) => {
+        console.log(({...obj1, ...obj2}));
+    };
+    mergeObjects({a : 1, b : 2}, {c : 3, d : 4});  
+    
