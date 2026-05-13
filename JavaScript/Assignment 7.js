fa53fa77a402d93e65040e885ca7454c98121d2e
kushagra9926 <joshikushagra899@gmail.com>
2026-05-13 12:45:28 +0530

    docs: add questions for Assignment 7 in JavaScript file

---
+++ JavaScript/Assignment 7.js
@@ -0,0 +1,5 @@
+// Question 1: Square and sum the array elements using the arrow function and then find the average of the array.
+// Question 2: Create a new array using the map function whose each element is equal to the original element plus 5.
+// Question 3: Create a new array whose elements are in upper case of words present in the original array.
+// Question 4: Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of the additional arguments doubled.
+// Question 5: Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
