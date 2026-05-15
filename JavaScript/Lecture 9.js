a7c2afefa59f5a541598eb987cc7d3e2e334c515
kushagra9926 <joshikushagra899@gmail.com>
2026-05-15 13:26:16 +0530

    docs: add Lecture 9 HTML, CSS, and JavaScript files

---
+++ JavaScript/Lecture 9.js
@@ -0,0 +1,19 @@
+// DOM (Document Object Model)
+/*
+It is basically a way for JavaScript to access and change HTML and CSS on a webpage.
+When a browser loads an HTML page, it converts the page into a tree-like structure called the DOM.
+
+DOM structure:
+Document
+ └── body
+      ├── h1
+      └── button
+
+JavaScript can use the DOM to:
+
+---> Change text
+---> Change styles
+---> Add/remove elements
+---> Handle button clicks
+---> Create animations
+*/
