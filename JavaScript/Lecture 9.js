a6394b3aa2a7539e0390d4bb694846f65b8b52d2
kushagra9926 <joshikushagra899@gmail.com>
2026-06-03 14:14:53 +0530

    docs: add practice question for DOM manipulation in Lecture 9.js

--- JavaScript/Lecture 9.js
+++ JavaScript/Lecture 9.js
@@ -456,5 +456,11 @@ parent.removeChild(child);
 
 // Practice Question
 /*
-
+Add the following elements to the container using only JavaScript and the DOM methods.
+1) a <p> with red text that says "Hey I'm red!"
+2) an <h3> with blue text that says "I'm a blue h3!"
+3) a <div> with a black border and pink background color with the following elements inside it:
+    ● another <h1> that says "I'm in a div"
+    ● a <p> that says "ME TOO!"
 */
+
