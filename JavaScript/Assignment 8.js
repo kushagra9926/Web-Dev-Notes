e989cf6775e084644e4ee2e2b08715ca8173b07e
kushagra9926 <joshikushagra899@gmail.com>
2026-06-05 15:16:32 +0530

    docs: add questions for Assignment 8 JavaScript tasks

---
+++ JavaScript/Assignment 8.js
@@ -0,0 +1,9 @@
+// Qs 1. Create a new input and button element on the page using Java Script only. Set the text of button to “Click me”;
+/* Qs 2. Add following attributes to the element:
+          - Change placeholder value of input to “username”
+          - Change the id of button to “btn”
+*/
+
+// Qs 3. Access the btn using the querySelector and button id. Change the button background color to blue and text color to white.
+// Qs 4. Create an h1 element on the page and set its text to “DOM Practice” underlined. Change its color to purple.
+// Qs 5. Create a p tag on the page and set its text to “Apna College Delta Practice”, where Delta is bold.
