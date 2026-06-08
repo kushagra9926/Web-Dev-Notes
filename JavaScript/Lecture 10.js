66be216463bdbd4cfbf8d8c0ae8c9a86fdd893f4
kushagra9926 <joshikushagra899@gmail.com>
2026-06-08 13:29:42 +0530

    docs: add button element and event handling example for Lecture 10

---
+++ JavaScript/Lecture 10.js
@@ -0,0 +1,7 @@
+// DOM Events
+/*
+DOM Events (Document Object Model Events) are actions or occurrences that happen in the browser, which JavaScript can detect and respond to.
+
+</> HTML
+<button onclick = "console.log("button was click")">click me</button>  
+*/
