e024eda28b318d668e62fc64dbdcf7651679a4f7
kushagra9926 <joshikushagra899@gmail.com>
2026-06-08 13:45:28 +0530

    docs: update button elements and event handling in Lecture 10

--- JavaScript/Lecture 10.js
+++ JavaScript/Lecture 10.js
@@ -5,3 +5,22 @@ DOM Events (Document Object Model Events) are actions or occurrences that happen
 </> HTML
 <button onclick = "console.log("button was click")">click me</button>  
 */
+
+// onclick Event in JavaScript
+/*
+The onclick event is triggered when a user clicks on an HTML element.
+*/
+let btns = document.querySelectorAll("button");
+for(btn of btns){
+    btn.onclick = sayHello;
+}
+
+// btn.onclick = function (){
+//     console.log("button was clicked");
+//     alert("button was clicked");
+// };
+
+function sayHello(){
+    alert("Hello!");
+} 
+
