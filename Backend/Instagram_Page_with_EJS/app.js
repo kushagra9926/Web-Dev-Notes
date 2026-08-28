d55d9e848d71bac919e72906c0d3f7f1300151e0
kushagra9926 <joshikushagra899@gmail.com>
2026-08-28 20:28:59 +0530

    Add initial setup for Instagram page with EJS
    - Created package.json with dependencies for EJS and Express.
    - Added error.ejs view for handling account not found scenarios.
    - Implemented instagram.ejs view to display user profile and posts.

---
+++ Backend/Instagram_Page_with_EJS/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let port = 8080;
+
+// app.use((req, res) => {
+//     console.log("request received")
+// });
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;  
+    const instaData = require("./data.json");
+    // console.log(instaData); 
+    const data = instaData[username]; 
+    if(data) {
+        res.render("instagram.ejs", { data });
+    } else{
+        res.render("error.ejs");
+    }
+    
+});
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
