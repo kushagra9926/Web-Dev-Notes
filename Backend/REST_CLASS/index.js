0bef7302e25347c799f92de91bf134b2d9cb1a2b
kushagra9926 <joshikushagra899@gmail.com>
2026-08-30 22:33:43 +0530

    feat: upgrade uuid package to version 14.0.2 and implement new UUID functionalities
    - Added new UUID generation methods including v1, v3, v4, v5, v6, and v7.
    - Introduced functions for UUID validation and conversion between versions.
    - Enhanced the stringify and parse functionalities for UUIDs.
    - Updated package.json to include uuid as a dependency.
    - Created new views for displaying and creating posts in the application.
    - Improved the index view with links to post details and new post creation.

--- Backend/REST_CLASS/index.js
+++ Backend/REST_CLASS/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 8080;
-
+const { v4: uuidv4} = require('uuid');
 app.use(express.urlencoded({ extended : true }));
 
 app.set("view engine", "ejs");
@@ -12,14 +12,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id: uuidv4(),
         username : "Kushagra",
         content : "I AM the BEST!",    
     },
     {
+        id: uuidv4(),
         username : "Raj",
         content : "I AM a SUCKER!",    
     },
     {
+        id: uuidv4(),
         username : "Shivansh",
         content : "I AM a GOONER!",    
     },
@@ -29,6 +32,22 @@ app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 });
 
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let { username, content} = req.body;
+    let id = uuidv4();
+    posts.push({ id, username, content });
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", { post });
+});
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
