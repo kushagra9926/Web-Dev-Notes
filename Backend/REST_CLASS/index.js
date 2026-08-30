39174946587279533907dd20a84e78b8f7abd64e
kushagra9926 <joshikushagra899@gmail.com>
2026-08-30 11:20:29 +0530

    feat: implement posts endpoint and add EJS template for displaying posts

--- Backend/REST_CLASS/index.js
+++ Backend/REST_CLASS/index.js
@@ -8,11 +8,27 @@ app.use(express.urlencoded({ extended : true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("server working well!");
+let posts = [
+    {
+        username : "Kushagra",
+        content : "I AM the BEST!",    
+    },
+    {
+        username : "Raj",
+        content : "I AM a SUCKER!",    
+    },
+    {
+        username : "Shivansh",
+        content : "I AM a GOONER!",    
+    },
+]
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
 });
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
