0e1bd7a05b008122186dbe5701935a94dba1ae14
kushagra9926 <joshikushagra899@gmail.com>
2026-08-31 23:11:22 +0530

    fix: add missing newline at end of index.js

--- Backend/REST_CLASS/index.js
+++ Backend/REST_CLASS/index.js
@@ -71,6 +71,7 @@ app.delete("/posts/:id", (req, res) => {
     posts = posts.filter((p) => id !== p.id);
     res.redirect("/posts");
 });
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
