a48f55154a90a11427e48b994d8eb7a1d6dc7da2
kushagra9926 <joshikushagra899@gmail.com>
2026-08-31 23:10:28 +0530

    feat: add delete functionality for posts

--- Backend/REST_CLASS/index.js
+++ Backend/REST_CLASS/index.js
@@ -66,6 +66,11 @@ app.get("/posts/:id/edit", (req, res) => {
     res.render("edit.ejs", { post });
 });
 
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+});
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
